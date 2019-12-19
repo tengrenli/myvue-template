f7ef61fdca27663f03967c0903df8b6e4edb797c
tengrenli <tengrenli@gokuaidian.com>
2019-12-19 09:32:17 +0800

    chore:配置vue模板-mock数据

--- template/src/views/home/index.vue
+++ template/src/views/home/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../../assets/logo.png">
-    <div>{{"15631709035" | formatPhone}}</div>
     <div class="test">
       <p class="hello">Hello</p>
     </div>
